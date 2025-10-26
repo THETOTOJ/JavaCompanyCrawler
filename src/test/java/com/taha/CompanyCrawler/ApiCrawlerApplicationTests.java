802f40c60e0249521219af3854787e9e9f07038f
Totoj <thetotoj@gmail.com>
2025-10-26 12:18:04 +0100

    renamed app

--- src/test/java/com/taha/CompanyCrawler/ApiCrawlerApplicationTests.java
+++ src/test/java/com/taha/CompanyCrawler/ApiCrawlerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiCrawlerApplicationTests {
+class CompanyCrawlerApplicationTests {
 
 	@Test
 	void contextLoads() {
