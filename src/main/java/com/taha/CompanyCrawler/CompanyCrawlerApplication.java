f8643cd010cd6e5859c5a967cd225296d8f6ec4f
Totoj <thetotoj@gmail.com>
2025-10-26 10:28:34 +0100

    Spring Boot and dependencies setup

---
+++ src/main/java/com/taha/CompanyCrawler/CompanyCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.taha.CompanyCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taha/CompanyCrawler/ApiCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taha.CompanyCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
